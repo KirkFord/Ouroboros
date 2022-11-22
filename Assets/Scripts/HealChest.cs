46f5a4f8f2610dd2aaeacf274a76268f57fa0963
Sarah Kostuk <slk612@usask.ca>
2022-11-21 20:13:53 -0600

    Healing Chest

---
+++ Assets/Scripts/HealChest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestHealButton : MonoBehaviour
+{
+   private CampFire campfire;
+
+   private void ChestHeal() {
+    campfire.Heal();
+   }
+}
