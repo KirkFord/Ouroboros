5917c6db4d80a50edb4b5cecdbacb11b3e217bd3
Cuba Giesbrecht <banishlight@gmail.com>
2022-12-07 14:28:47 -0600

    Garlic works!

--- Assets/Scripts/Weapon_Garlic.cs
+++ Assets/Scripts/Weapon_Garlic.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Weapon_Garlic : MonoBehaviour
 {
+    private int cdMax = 40;
+    private int cdCurrent = 0;
+    [SerializeField] private float damage = 50.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,29 @@ public class Weapon_Garlic : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.transform.tag == "Enemy")
+        {
+            if (cdCurrent <= 0)
+            {
+                DealDamage(other);
+                cdCurrent = cdMax;
+            }
+            else
+            {
+                cdCurrent -= 1;
+            }
+        }
+    }
+
+    void DealDamage(Collider other)
+    {
+        other.GetComponent<Enemy>().TakeDamage(damage, false);
     }
 }
