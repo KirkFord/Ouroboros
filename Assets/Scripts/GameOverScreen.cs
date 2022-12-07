c986104681c63126f8d1daf07c4b62ff0d41227d
Kirk Ford <kirkford@hotmail.ca>
2022-12-07 14:30:35 -0600

    fixed game over screen issues

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -6,9 +6,12 @@ public class GameOverScreen : MonoBehaviour
     public StatScreen stats;
     //public static bool GamePaused = false;
     public GameObject gameOverUI;
-    
+    public GameObject mainUI;
+
     public void Dead()
     {
+        mainUI.GetComponent<MainUi>().resetFlash();
+        mainUI.SetActive(false);
         gameOverUI.SetActive(true);
         Time.timeScale = 0f;
     }
