b20ded378d40871273a0e22711729d949e4620bd
Noah <36053916+noahbesse@users.noreply.github.com>
2022-12-07 23:36:33 -0600

    balanc
    :D

--- Assets/Scripts/chonkreward.cs
+++ Assets/Scripts/chonkreward.cs
@@ -6,6 +6,6 @@ public class chonkreward : MonoBehaviour
     {
         var chonkReward = 50 * (GameManager.Instance.GetLoops() / 4);
         CoinManager.Instance.AddCoins(chonkReward);
-        DamagePopup.CreatePlayerPopup(Player.Instance.transform.position, $"+{chonkReward}");
+        DamagePopup.CreatePlayerPopup(Player.Instance.transform.position, $"+{chonkReward} Coins");
     }
 }
