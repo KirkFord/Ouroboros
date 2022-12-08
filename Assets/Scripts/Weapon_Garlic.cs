c4a5b877d48bdbd7b7fa907dd40d85a548de2292
Cuba Giesbrecht <banishlight@gmail.com>
2022-12-07 18:17:37 -0600

    Garlic Rework

--- Assets/Scripts/Weapon_Garlic.cs
+++ Assets/Scripts/Weapon_Garlic.cs
@@ -7,31 +7,61 @@ public class Weapon_Garlic : MonoBehaviour
     private int cdMax = 40;
     private int cdCurrent = 0;
     [SerializeField] private float damage = 50.0f;
+    private List<Collider> collList;
     // Start is called before the first frame update
+
+    void Start()
+    {
+        collList = new List<Collider>();
+    }
+
 
     public void AddDamage(float extraDamage)
     {
         damage += extraDamage;
     }
 
-    void OnTriggerStay(Collider other)
+    public void FixedUpdate()
     {
-        if(other.transform.tag == "Enemy")
+        if (cdCurrent <= 0)
         {
-            if (cdCurrent <= 0)
-            {
-                DealDamage(other);
-                cdCurrent = cdMax;
-            }
-            else
-            {
-                cdCurrent -= 1;
-            }
+            DealDamage();
+            cdCurrent = cdMax;
+        }
+        else
+        {
+            cdCurrent -= 1;
         }
     }
 
-    void DealDamage(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<Enemy>().TakeDamage(damage, false);
+        if (other.transform.tag == "Enemy")
+        {
+            collList.Add(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Enemy")
+        {
+            collList.Remove(other);
+        }
+    }
+
+    void DealDamage()
+    {
+        for (int i = 0; i < collList.Count; i++)
+        {
+            if (collList[i] == null)
+            {
+                collList.Remove(collList[i]);
+            }
+            else
+            {
+                collList[i].GetComponent<Enemy>().TakeDamage(damage, false);
+            }
+        }
     }
 }
